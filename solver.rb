ca588ee09e8e04e9745fb7cf17b15af72dcecae5
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-03-10 14:47:54 -0500

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,7 @@
 class Solver
+
+  def factorial(number)
+    return 1 unless number > 1
+    number * factorial(number - 1)
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,14 +1,13 @@
 require_relative '../solver'
 
 describe Solver do
-    before :each do
-        @solver = Solver.new
-    end
+  before :each do
+    @solver = Solver.new
+  end
 
-    describe 'Calculate the factorial of the number' do
-        it 'return the factorial of the number' do
-            expect(@solver.factorial(5)).to eql (120)
-        end
+  describe 'Calculate the factorial of the number' do
+    it 'return the factorial of the number' do
+      expect(@solver.factorial(5)).to eql(120)
     end
-
+  end
 end
